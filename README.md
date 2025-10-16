787f3e16f9e6b50d6077152671c70342da33db75
Grant Roode <109985613+Cat5-0@users.noreply.github.com>
2025-10-16 14:47:37 +0200

    Step 5 trying to playRound

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 Pseudocode Input
 A program that creates a game played entirely in the console:
     1 Game played against the computer.
-    2 Step 1 Setup Project Structute
+    2 Step 1 Setup Project Structure
       A function that randomly returns "rock", "paper" or "scissors".
 
     3 Step 2 Create a new function named getComputerChoice
@@ -27,17 +27,38 @@ A program that creates a game played entirely in the console:
     11 Create variable humanScore in the global scope
     12 Create variable computerScore in the global scope
     13 Initialize these variables with the value 0
-    14
+
+    14 Step 5 Write the logic to play a single round
+    15 Create a function to take human and computer choices as arguments 
+    16 Play a single round
+    17 Increments the round winner's score
+    18 Logs a winner announcement
+
+
 
 
 - Plan
 
 Pseudocode Output
 Calculate random number range 1 to 3
-Create new function named getComputerChoice
-If randomNumber is 1 console.log "Rock"
-If randomNumber is 2 console.log "Paper"
-If randomNumber is 3 console.log "Scissors"
+Create function getComputerChoice
+IF randomNumber is 1 console.log "Rock"
+IF randomNumber is 2 console.log "Paper"
+IF randomNumber is 3 console.log "Scissors"
+Console.log tests that working correctly
+Create function getHumanChoice
+getHumanChoice to return valid choice
+Console.log tests that working correctly
+Declare humanScore and initialize to 0
+Declare computerScore and initialize to 0
+//Play a single round
+Create Function playRound
+Define parameters humanChoice and computerChoice for playRound 
+Parameters to take human and computer choices as arguments
+humanChoice parameter case-insensitive
+write code for playRound to console.log a string value for round winner
+"You lose! Paper beats Rock"
+Increment humanScore or computerScore variable based on the round winner
 
 
 
--- script.js
+++ script.js
@@ -3,43 +3,62 @@
   // Declare variables for keeping scores
   let humanScore = 0;
   let computerScore = 0;
-  
+
   // Prompt for user to enter their choice
-  let humanChoice = prompt("Please enter Rock, Paper or Scissors:  ");
+  let humanInput = prompt("Please enter Rock, Paper or Scissors:");
 
   // Random number generated
   let randomNumber = Math.floor(Math.random() * 3) + 1;
-  console.log("Computer", randomNumber);
+  console.log("Computer Number", randomNumber);
 
-  // Variable for user choice assigned to human number
+  // Variable for user choice assigned to human number I added to verify 
+  // which console.log is human or computer
   let humanNumber;
+  let randomName;
 
   // Function for computer choice that displays assigned name 
   // of generated random number 
-  function getComputerChoice() {
+  function getComputerInput() {
     if (randomNumber == 1) {
-        console.log("Computer" + " " + "Rock");
+        randomName = "Rock";
+        console.log("Computer Rock");
     } else if (randomNumber === 2) {
-        console.log("Computer" + " " + "Paper");
+        randomName = "Paper";
+        console.log("Computer Paper");
     } else {
-        console.log("Computer" + " " + "Scissors");
+        randomName = "Scissors";
+        console.log("Computer Scissors");
     }
   }
-  getComputerChoice();
+  getComputerInput();
 
   // Function to display human choice that was prompted for
-  function getHumanChoice() {
-    if (humanChoice === "Rock") {
+  function getHumanInput() {
+    if (humanInput === "Rock") {
         humanNumber = 1;
-        console.log("Rock");
+        console.log("Human Rock");
         console.log("Human Number", humanNumber);
-    } else if (humanChoice === "Paper") {
+    } else if (humanInput === "Paper") {
         humanNumber = 2;
-        console.log("Paper");
-    } else if (humanChoice === "Scissors") {
+        console.log("Human Paper");
+        console.log("Human Number", humanNumber);
+    } else if (humanInput === "Scissors") {
         humanNumber = 3;
-        console.log("Scissors");
-    }
+        console.log("Human Scissors");
+        console.log("Human Number", humanNumber);
+    }computerChoice
   }
-  getHumanChoice();
+  getHumanInput();
 
+  // Function takes human and computer choices as argumets
+  // plays a single round, increments the round winner score 
+  // and logs a winner announcement
+
+  let humanChoice = humanInput.toLowerCase();
+  let computerChoice = randomName.toLowerCase()
+
+  function playRound(humanChoice, computerChoice) {
+    console.log(humanChoice, randomName);
+  }
+  playRound();
+ 
