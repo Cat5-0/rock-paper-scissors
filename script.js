53b127fb43c8c738f14731c716235614542223b7
Grant Roode <109985613+Cat5-0@users.noreply.github.com>
2025-10-16 11:01:59 +0200

    Return Rock Paper Scissors

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
   console.log("Hello World");
-  
+
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+  console.log(randomNumber);
+
+  function getComputerChoice() {
+    if (randomNumber == 1) {
+        console.log("Rock");
+    } else if (randomNumber === 2) {
+        console.log("Paper");
+    } else {
+        console.log("Scissors");
+    }
+  }
+  getComputerChoice();
