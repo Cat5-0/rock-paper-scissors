0304cd4486e9fe9f8cdc9d652970ea1fe3c8b0fc
Grant Roode <109985613+Cat5-0@users.noreply.github.com>
2025-11-12 17:48:59 +0200

    Updating index.html up to playRound

--- main.js
+++ main.js
@@ -1,2 +1,110 @@
 // Rock Paper Scissors
 
+// Variables to keep track of player scores
+let playerScore = 0;
+let computerScore = 0;
+let tiedScore = 0;
+
+// Select HTML elements
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
+const resetGame = document.getElementById("resetGame");
+
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+const tiedScoreDisplay = document.getElementById("tiedScoreDisplay");
+
+// Add event listeners to the buttons
+rockButton.addEventListener('click', () => playRound('rock'));
+paperButton.addEventListener('click', () => playRound('paper'));
+scissorsButton.addEventListener('click', () => playRound('scissor'));
+
+// Main game function
+function playRound(playerChoice) {
+    console.log(`Player choice ${playerChoice}`)
+    const computerChoice = getComputerChoice();
+    // To be added
+    // const winner = determineWinner(playerChoice, computerChoice);
+    // updateScore(winner);
+    // updateMessage(winner, playerChoice, computerChoice);
+}
+
+// Function to generate computer random choice 
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    console.log(`Computer selected ${computerChoice}`);
+    return computerChoice;
+}
+
+// Function checking who the winner is comparing choices
+
+function checkWinner(player, computer) {
+    if (player === computer) {
+        return "Tie";
+    } else if (
+        (player === "rock" && computer === "scissors") ||
+        (player === "paper" && computer === "rock") ||
+        (player === "scissors" && computer === "paper")
+    ) {
+        humanScore += 1;
+        return "Human";
+    } else {
+        computerScore += 1;
+        return "Computer";
+    }
+}
+
+
+
+    //Calculate first to reach 5 games and win
+    // if (playerScore === 5 || computerScore === 5) {
+    //     if (playerScore === computerScore) {
+    //         resultDisplay.textContent = ("Game over Tied");
+    //         resetGame();
+    //         return;
+    //     } else if (playerScore > computerScore) {
+    //         resultDisplay.textContent = ("Game over Human won");
+    //         resetGame();
+    //         return;
+    //     } else {
+    //         resultDisplay.textContent = ("Game over Computer won");
+    //         resetGame();
+    //         return;
+    //     }
+    // }
+
+
+    // Display results after each game
+    // playerDisplay.textContent = `Human: ${playerChoice}`;
+    // computerDisplay.textContent = `Computer: ${computerChoice}`;
+    // resultDisplay.textContent = result;
+
+    // Allocate scores against results
+    // resultDisplay.classList.remove("greenText", "redText")
+    // switch(result) {
+    //     case "You win!":
+    //         resultDisplay.classList.add("greenText");
+    //         playerScore++;
+    //         playerScoreDisplay.textContent = playerScore;
+    //         break;
+    //     case "You lost":
+    //         resultDisplay.classList.add("redText");
+    //         computerScore++;
+    //         computerScoreDisplay.textContent = computerScore;
+    //         break;
+    //     case "Tied!":
+    //         tiedScore++;
+    //         tiedScoreDisplay.textContent = tiedScore;
+    //         break;
+    // }
+
+// function resetGame() {
+//     console.log("Game is over");
+// }
+
